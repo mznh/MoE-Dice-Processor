e0e4505787f6e328b9fb33fc1fe06d3b6740c834
mznh <hiromtym@gmail.com>
2020-03-01 18:31:52 +0900

    hogehoge

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@
 require "bundler/setup"
 Bundler.require
 
+require 'date'
+
 set :environment, :production
 set :port, 7777
 
@@ -10,14 +12,21 @@ UPLOAD_PATH = "./logs/"
 
 helpers  do
   def log_process data, config
-    regexp = /([0-9\/ :]+) ([^ ]+) はマジックダイス\(0-1000\)を振った！ ([0-9]+) が出た！/
+    regexp = /([0-9\/ :]+) ([a-zA-Zぁ-んァ-ヶー－]+) はマジックダイス\(0-1000\)を振った！ ([0-9]+) が出た！/
     tmp = data.select{|line|line.include? "マジックダイス"}
         .map{|rec|regexp.match(rec)}.select{|i|!i.nil?}
         .map{|rec|rec.to_a.drop(1)}
+    p tmp
+    ## 最新のダイス時刻を取得　
+    last_time = tmp.map{|rec| DateTime.parse(rec[0].split(/ /)[1]) }.sort.last
+    ## 最新ダイス時刻と差が 1 hour 以上のものを削除
+    ## うーんリファクタリング候補
+    ## DateTimeの-がRationalを返すのでごにょごにょ
+    tmp = tmp.select{|rec| (last_time - DateTime.parse(rec[0].split(/ /)[1]))*24*60*60.to_i <= 60*60 }
+
     if config.fetch(:order,nil) == "asc" then
       return tmp.sort_by{|i|i[2].to_i}
     else 
-      p  tmp.sort_by{|i|i[2].to_i}.reverse
       return tmp.sort_by{|i|i[2].to_i}.reverse
     end
   end
